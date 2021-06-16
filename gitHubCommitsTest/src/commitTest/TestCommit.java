ada4fc56cb99904cb2580493b7aaf48793642cb0
carlcitchen <carlcitchen@localhost>
2021-06-16 14:26:20 -0500

    Pushing to Github for the first time for Claim Academy

---
+++ gitHubCommitsTest/src/commitTest/TestCommit.java
@@ -0,0 +1,11 @@
+package commitTest;
+
+public class TestCommit {
+
+	public static void main(String[] args) {
+
+		System.out.println("This is an attempt to commit a project to GitHub");
+	
+	}
+
+}
